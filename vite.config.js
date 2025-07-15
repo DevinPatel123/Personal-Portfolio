65ba313e7b826cf65538fd11b698fbe3cf798598
DevinPatel123 <dpatel021306@gmail.com>
2025-07-15 16:44:59 -0500

    Trigger deploy

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Personal-Portfolio/', // Set base path for deployment
+  base: '/', // Set base path for root deployment
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
